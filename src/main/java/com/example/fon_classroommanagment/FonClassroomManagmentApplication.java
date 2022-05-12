c17304ec497f515d25cb50c23ad7f745a2462e08
ilijaradojkovic <47418955+ilijaradojkovic@users.noreply.github.com>
2022-05-13 01:34:27 +0300

    Initial commit

---
+++ src/main/java/com/example/fon_classroommanagment/FonClassroomManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.fon_classroommanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonClassroomManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FonClassroomManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fon_classroommanagment/FonClassroomManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fon_classroommanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonClassroomManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
