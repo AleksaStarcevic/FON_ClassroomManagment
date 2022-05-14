758613f46f45b691a2c0e8995d21d1685bcd3471
ilijaradojkovic <47418955+ilijaradojkovic@users.noreply.github.com>
2022-05-14 16:54:45 +0200

    Dodate filter klase: BeforeRequestTokenFilter i UserFilter

---
+++ src/main/java/com/example/fon_classroommanagment/Filters/UserFilter.java
@@ -0,0 +1,6 @@
+package com.example.fon_classroommanagment.Filters;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class UserFilter extends UsernamePasswordAuthenticationFilter {
+}
