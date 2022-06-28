07417e2892493e5ad4dd66e26fffa8b7f834fa26
ilijaradojkovic <radojkovicika@gmail.com>
2022-06-28 17:51:03 +0200

    Dodato da AppointmentRequestedUserDTO vraca i id appointmenta

--- src/main/java/com/example/fon_classroommanagment/Repository/AppointmentRepository.java
+++ src/main/java/com/example/fon_classroommanagment/Repository/AppointmentRepository.java
@@ -79,6 +79,6 @@ public interface AppointmentRepository extends JpaRepository<Appointment, UUID> 
     @Query("select t from Appointment  t where t.date =:date and t.classroom.id=:classroomId")
     List<Appointment> findByDateAndClassroom(@Param("date") Date date,@Param("classroomId") Long classroomId);
 
-    @Query("select  new com.example.fon_classroommanagment.Models.DTO.AppointmentRequestedUserDTO(p.classroom.name,p.name,p.date,p.Start_timeInHours,p.End_timeInHours) from Appointment  p where p.employee.id=:emlopyeeId and p.status.id=:status")
+    @Query("select  new com.example.fon_classroommanagment.Models.DTO.AppointmentRequestedUserDTO(p.id,p.classroom.name,p.name,p.date,p.Start_timeInHours,p.End_timeInHours) from Appointment  p where p.employee.id=:emlopyeeId and p.status.id=:status")
     List<AppointmentRequestedUserDTO> findByeAndEmployeeIdAndStatus(@Param("emlopyeeId") Long emlopyeeId,@Param("status")Long status);
 }
