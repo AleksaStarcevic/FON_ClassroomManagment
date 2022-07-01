d57f6f88189eef51ef101a66b3926cc74d68aa42
ilijaradojkovic <radojkovicika@gmail.com>
2022-07-01 19:05:28 +0200

    Promena naziva

--- src/main/java/com/example/fon_classroommanagment/Configuration/ExceptionMessages.java
+++ src/main/java/com/example/fon_classroommanagment/Configuration/ExceptionMessages.java
@@ -2,7 +2,7 @@ package com.example.fon_classroommanagment.Configuration;
 
 public class ExceptionMessages {
 
-    public static final String USER_EXISTS = "User does not exists";
+    public static final String USER_EXISTS = "Employee does not exists";
     public static final String USERNAME_NOT_FOUND = "Please register,user does not exit";
     public static final String TOKEN_EXPIRED = "Please login again,there is no user with given email";
 
